240e6021047ade2f79d5be847a6e58a58a0b4ea6
Martin Hochel <hochelmartin@gmail.com>
2017-03-27 19:53:53 +0200

    refactor: use ts-config-standard

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,6 +1,6 @@
 export class Greeter {
   constructor(private greeting: string) { }
   greet() {
-    return `Hello, ${this.greeting}!`;
+    return `Hello, ${this.greeting}!`
   }
 }
--- src/__tests__/Greeter.spec.ts
+++ src/__tests__/Greeter.spec.ts
@@ -1,20 +1,20 @@
-import { Greeter } from '../Greeter';
+import { Greeter } from '../Greeter'
 
 describe(`Greeter`, () => {
 
-  let greeter: Greeter;
+  let greeter: Greeter
 
   beforeEach(() => {
-    greeter = new Greeter('World');
-  });
+    greeter = new Greeter('World')
+  })
 
   it(`should greet`, () => {
 
-    const actual = greeter.greet();
-    const expected = 'Hello, World!';
+    const actual = greeter.greet()
+    const expected = 'Hello, World!'
 
-    expect(actual).toBe(expected);
+    expect(actual).toBe(expected)
 
-  });
+  })
 
-});
+})
